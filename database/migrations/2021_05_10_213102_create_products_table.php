6192ce3b5aa3b35c0d2753de94a5e0f98ff8f385
leonsantiago <santiagoleon747@gmail.com>
2021-05-10 18:42:43 -0300

    Categories and products migration

--- database/migrations/2021_05_10_213102_create_products_table.php
+++ database/migrations/2021_05_10_213102_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,14 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories');
             $table->string('name')->unique();
+            $table->string('unit');
+            $table->float('price');
+            $table->boolean('active');
+
             $table->timestamps();
         });
     }
@@ -27,6 +32,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 }
